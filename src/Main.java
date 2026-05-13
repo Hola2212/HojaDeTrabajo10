5895a74073f5e9cfa61b3ddfdeafc572970313a5
Hola2212 <juanpaflores06@gmail.com>
2026-05-13 06:52:48 -0600

    Finished main menu.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,45 @@
-import java.io.File;
-import java.util.Vector;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Vector<Token> tokens = new FileReaderUtil().ReadFile("reosurses/guategrafo.txt");
-
+        Vector<Token> tokens = new FileReaderUtil().ReadFile("src/resources/guategrafo.txt");
+        Graph g = new Graph(tokens);
+        g.floyd();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Ruta más corta");
+            System.out.println("2. Centro del grafo");
+            System.out.println("3. Modificar grafo");
+            System.out.println("4. Salir");
+            int op = sc.nextInt();
+            if (op == 1) {
+                System.out.print("Origen: ");
+                String a = sc.next();
+                System.out.print("Destino: ");
+                String b = sc.next();
+                System.out.println("Ruta: " + g.getPath(a, b));
+                System.out.println("Distancia: " + g.getDistance(a, b));
+            }
+            else if (op == 2) {
+                System.out.println("Centro: " + g.getCenter());
+            }
+            else if (op == 3) {
+                System.out.println("1. Eliminar conexión");
+                System.out.println("2. Agregar conexión");
+                int sub = sc.nextInt();
+                if (sub == 1) {
+                    String a = sc.next();
+                    String b = sc.next();
+                    g.removeEdge(a, b);
+                } else {
+                    String a = sc.next();
+                    String b = sc.next();
+                    float d = sc.nextFloat();
+                    g.addEdge(a, b, d);
+                }
+                g.floyd();
+            }
+            else break;
+        }
     }
 }
